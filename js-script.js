d08e02d0cb5495e56fa82fc53262551540af03f5
sporkdelis <117935904+sporkdelis@users.noreply.github.com>
2022-11-12 08:47:41 -0600

    Update js-script.js

--- js-script.js
+++ js-script.js
@@ -5,11 +5,9 @@ document.getElementById ("jshit") .style .color = "white";
 
 /* use the canvas div as the holding container for animations */
 
-document.getElementById ("canvas").onclick = function(){
-  
-  };
+/* create Ball object*/
 
-var Ball = function(x,y){
+function Ball (x,y){
   this.x = x;
   this.y = y;
   };
@@ -20,13 +18,22 @@ Ball.prototype.draw = function (){
   };
   
 Ball.prototype.bounce = function () {
-  for(i = 0; i > 375; i++);
+  for(i = 0; i > 375; i++){
   if(i === 375) {
     while( i < 25) {
       i--;
       }
     }
-  x = x + i;
-  y = mouseY;
+  x = i;
+  }
+  };
+  
+  /* set up draw function to draw the ball where the user clicks within canvas*/
+  
+  draw(){
+  document.getElementById ("canvas").onclick = function(){
+      var ball = new Ball;
+      ball.draw(0,mouseY);
+   }
   };
   
