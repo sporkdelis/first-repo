05d59f44905aa885305998fb1e0c4386afc33f54
sporkdelis <117935904+sporkdelis@users.noreply.github.com>
2022-11-12 08:50:09 -0600

    Update js-script.js

--- js-script.js
+++ js-script.js
@@ -1,7 +1,7 @@
 /* my first javascript code practice   */
 
-document.getElementById ("jshit").style.backgroundColor = "navy";
-document.getElementById ("jshit").style.color = "white";
+document.getElementById("jshit").style.backgroundColor = "navy";
+document.getElementById("jshit").style.color = "white";
 
 /* use the canvas div as the holding container for animations */
 
